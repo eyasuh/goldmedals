9338404827a6cced3c0f95356ba4d80e9790ba0d
eyasuh <eyha12@gmail.com>
2022-07-26 16:13:18 -0400

    custome queries for getCountryMedalsListResponse

--- src/main/java/com/codecademy/goldmedal/repositories/GoldMedalRepository.java
+++ src/main/java/com/codecademy/goldmedal/repositories/GoldMedalRepository.java
@@ -1,6 +1,18 @@
 package com.codecademy.goldmedal.repositories;
 
+import com.codecademy.goldmedal.model.GoldMedal;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface GoldMedalRepository extends CrudRepository {
+	List<GoldMedal> findByYear();
+
+	List<GoldMedal> findByEvent();
+
+	List<GoldMedal> findByName();
+
+	List<GoldMedal> findByCity();
+
+	List<GoldMedal> findBySeason();
 }
