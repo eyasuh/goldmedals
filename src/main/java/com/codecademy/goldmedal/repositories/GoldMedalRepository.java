a29f3c7e626467c8e913ea270559b5756bc310d6
eyasuh <eyha12@gmail.com>
2022-07-28 19:25:49 -0400

    GoldMedalRepository custom queries fixed

--- src/main/java/com/codecademy/goldmedal/repositories/GoldMedalRepository.java
+++ src/main/java/com/codecademy/goldmedal/repositories/GoldMedalRepository.java
@@ -5,14 +5,19 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface GoldMedalRepository extends CrudRepository {
-	List<GoldMedal> findByYear();
-
-	List<GoldMedal> findByEvent();
-
-	List<GoldMedal> findByName();
-
-	List<GoldMedal> findByCity();
-
-	List<GoldMedal> findBySeason();
+public interface GoldMedalRepository extends CrudRepository<GoldMedal, Long> {
+	List<GoldMedal> findByCountryOrderByYearAsc(String country);
+	List<GoldMedal> findByCountryOrderByYearDesc(String country);
+	List<GoldMedal> findByCountryOrderBySeasonAsc(String country);
+	List<GoldMedal> findByCountryOrderBySeasonDesc(String country);
+	List<GoldMedal> findByCountryOrderByCityAsc(String country);
+	List<GoldMedal> findByCountryOrderByCityDesc(String country);
+	List<GoldMedal> findByCountryOrderByNameAsc(String country);
+	List<GoldMedal> findByCountryOrderByNameDesc(String country);
+	List<GoldMedal> findByCountryOrderByEventAsc(String countryName);
+	List<GoldMedal> findByCountryOrderByEventDesc(String countryName);
+	List<GoldMedal> findByCountryAndSeasonOrderByYearAsc(String country, String season);
+	int countByCountry(String country);
+	long countBySeason(String season);
+	int countByCountryAndGender(String country, String gender);
 }
